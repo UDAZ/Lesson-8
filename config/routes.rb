8de29ffef052c52d09963654a7bc4f560a8fa7f6
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-05-04 18:22:21 +0000

    fix detail

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => 'users/registrations'}
   root 'homes#top'
   resources :users,only: [:show,:index,:edit,:update]
   resources :books
