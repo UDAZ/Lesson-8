bd54d9fb7ce27a1fc2896ffb8dffc996e79896b0
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-05-09 12:47:24 +0000

    add user

--- app/mailers/thanks_mailer.rb
+++ app/mailers/thanks_mailer.rb
@@ -1,6 +1,6 @@
 class ThanksMailer < ApplicationMailer
     def send_when_user_registration(user)
         @user = user
-        mail( to: user.email, subject: '【bookers】ご登録ありがとうございます')
+        mail to: user.email, subject: '【bookers】ご登録ありがとうございます'
     end
 end
